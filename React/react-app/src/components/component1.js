dfe982def7ff5d4c5d3b68d64c153beced2c0e69
sayf-u-khan <sayf.khan@sequenx.com>
2024-11-14 15:58:41 +0000

    modified:   React/react-app/src/App.js 	new file:   React/react-app/src/components/component1.js

---
+++ React/react-app/src/components/component1.js
@@ -0,0 +1,13 @@
+// src/MyFirstComponent.js
+import React from 'react';
+
+const ComponentOne = () => {
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+            <p>This is my first React component!</p>
+        </div>
+    );
+};
+
+export default ComponentOne;
