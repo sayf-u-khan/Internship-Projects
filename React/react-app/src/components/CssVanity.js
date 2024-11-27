4d5a0915ac5df6014abf798324770d67d4f6d701
sayf-u-khan <sayf.khan@sequenx.com>
2024-11-27 18:23:04 +0000

    "Updated CSS and JS files in React app: added new styles and classes, modified existing ones, and changed image and navigation elements."

--- React/react-app/src/components/CssVanity.js
+++ React/react-app/src/components/CssVanity.js
@@ -5,10 +5,13 @@ const CssVanity = () => {
     return (
         <div>
             <h1 class="h1c">This is the Title</h1>
-            <p class="box">This text is in a box</p>
-            <p class="highlight">This text is highlighted</p>
-            <p class="block">Block with padding</p>
-            <p class="inline">Inline text</p>
+            <p class="padded-text box">This text is in a box</p>
+            <p class="padded-text highlight">This text is highlighted</p>
+            <p class="padded-text block">Block with padding</p>
+            <p class="padded-text inline">Inline text</p>
+            <p class='padded-text section1'>Highlight gradient</p>
+            <p class='padded-text animate'>Animate gradient</p>
+            <p class='padded-text hover-effect'>Drop shadow hover</p>
         </div>
     );
 };
--- React/react-app/src/components/ImagePage.js
+++ React/react-app/src/components/ImagePage.js
@@ -2,8 +2,9 @@ import "./ImagePage.css";
 
 const ImagePage = () => {
     return (
+        <body>
         <div>
-            <img src="/images/image1.jpg" alt="Jade rabbit"></img>
+            <img class='imga' id="rcorners3" src="/images/image1.jpg" alt="Jade rabbit"></img>
             <h1>Image Things</h1>
             <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, 
@@ -16,12 +17,34 @@ const ImagePage = () => {
             venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh 
             tempor porta. Cras ac leo purus. Mauris quis diam velit.
             </p>
+            <h2 id="borderimg">Select this text!</h2>
+            <p>This is a navigation bar using image sprites</p>
             <ul id="navlist">
-            <li id="park"><a href="./ImagePage"></a></li>
-            <li id="building"><a href="./ImagePage"></a></li>
-            <li id="trees"><a href="./ImagePage"></a></li>
+            <li id="home"><a href=".">1</a></li>
+            <li id="prev"><a href="ImagePage">2</a></li>
+            <li id="next"><a href="ImagePage">3</a></li>
             </ul>
+            <div class="polaroid-container">
+            <div>
+            <h2>Responsive Polaroid Images / Cards</h2>
+            <div className="polaroid-container">
+                <div className="polaroid">
+                    <img src="/images/cubic.jpg" alt="Cubic" style={{ width: '100%' }} />
+                    <div className="container">
+                        <p>Cubic Houses</p>
+                    </div>
+                </div>
+                <div className="polaroid">
+                    <img src="/images/cubic.jpg" alt="Cubic" style={{ width: '100%' }} />
+                    <div className="container">
+                        <p>Cubic Houses</p>
+                    </div>
+                </div>
+            </div>
         </div>
+        </div>
+        </div>
+        </body>
     );
 };
 
